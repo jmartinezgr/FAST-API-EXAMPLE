dad843550cdbee1b9bc9199eae16861aa9a480c7
Juan <josemargri3@gmail.com>
2024-09-07 00:26:01 -0500

    feat: Se finalizan sus endpoints con su configuracion

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI CRUD",
+    openapi_tags=[{
+        "name": "User",
+        "description": "Endpoints related to users"
+    }]
+)
 
 app.include_router(user, prefix="/user")    
--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+from fastapi.responses import JSONResponse
 from config.db import conn
 from models.user import users
 from schemas.user import User
@@ -11,7 +12,7 @@ f = Fernet(key)
 # Instancia de APIRouter
 user = APIRouter()
 
-@user.get("/")
+@user.get("/",response_model=list[User])
 def get_users():
     result = conn.execute(users.select()).fetchall()
     
@@ -25,10 +26,10 @@ def get_users():
         
         for item in result
     ]
-    
-    return {"data": data}
+        
+    return JSONResponse(content=data, status_code=status.HTTP_200_OK)
 
-@user.get("/{id}")
+@user.get("/{id}", response_model=User)
 def get_user(id: int):
     result = conn.execute(users.select().where(users.c.id == id)).first()
     
@@ -39,11 +40,11 @@ def get_user(id: int):
             "email": result[2],
             "password": result[3]
         }
-        return {"data": user}
+        return JSONResponse(content=user, status_code=status.HTTP_200_OK)
     else:
-        return {"error": "User not found"}
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"error": "User not found"})
 
-@user.post("/")
+@user.post("/", response_model=User)
 def create_user(user: User):
     new_user = {
         "name": user.name,
@@ -66,6 +67,41 @@ def create_user(user: User):
             "email": user_created[2],
             "password": user_created[3]
         }
-        return user_dict
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=user_dict)
     else:
-        return {"error": "User not found"}
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"error": "User not found"})
+    
+@user.put("/{id}", response_model=User)
+def update_user(id: int, user: User):
+    new_user = {
+        "name": user.name,
+        "email": user.email,
+        "password": f.encrypt(user.password.encode("utf-8"))
+    }
+    
+    result = conn.execute(users.update().values(new_user).where(users.c.id == id))
+    conn.commit()
+    
+    if result.rowcount:
+        user_updated = conn.execute(users.select().where(users.c.id == id)).first()
+        
+        user_dict = {
+            "id": user_updated[0],
+            "name": user_updated[1],
+            "email": user_updated[2],
+            "password": user_updated[3]
+        }
+        
+        return JSONResponse(content=user_dict, status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"error": "User not found"})    
+    
+@user.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: str):
+    result = conn.execute(users.delete().where(users.c.id == id))
+    conn.commit()
+    
+    if result.rowcount:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,  content={"error": "User not found"})
